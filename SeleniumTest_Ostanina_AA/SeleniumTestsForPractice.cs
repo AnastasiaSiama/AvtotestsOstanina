524d7dc0ab67b4fb30c04814ba52fa1f779402f8
AnastasiaSiama <siama16@mail.ru>
2024-04-14 23:31:14 +0400

    first

---
+++ SeleniumTest_Ostanina_AA/SeleniumTestsForPractice.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest_Ostanina_AA;
+
+public class SeleniumTestsForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        //зайти в хром (с помощью драйвера)
+        var driver = new ChromeDriver(options);
+
+        //перейти по урлу https://staff-testing.testkontur.ru
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(5000);
+
+        // ввести логин и пароль
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("Siama16@mail.ru");
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("Tinctura92!");
+        Thread.Sleep(5000);
+
+        // нажать на кнопку "войти" 
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(5000);
+
+        // проверяем что пользователь на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+
+        // закрываем браузер и убиваем процесс драйвера
+        driver.Quit();
+    }
+}
