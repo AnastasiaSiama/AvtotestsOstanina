939bfcf021602497780d6c4b56b74941b44fd69c
AnastasiaSiama <siama16@mail.ru>
2024-04-23 12:02:25 +0400

    поправила по ревю

--- SeleniumTest_Ostanina_AA/SeleniumTestsForPractice.cs
+++ SeleniumTest_Ostanina_AA/SeleniumTestsForPractice.cs
@@ -1,26 +1,35 @@
-﻿using System.Drawing;
-using FluentAssertions;
-using FluentAssertions.Extensions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace SeleniumTest_Ostanina_AA;
 
 public class SeleniumTestsForPractice
 {
     private const string login = "Siama16@mail.ru";
-    public const string password = "Tinctura92!";
-    public ChromeDriver driver;
+    private const string password = "Tinctura92!";
+    private const string baseUrl = "https://staff-testing.testkontur.ru/";
+    private static readonly TimeSpan defaultTimeout = TimeSpan.FromSeconds(3);
+
+    private ChromeDriver driver;
+    private WebDriverWait waiter;
 
     [SetUp]
     public void Setup()
     {
-        var options = new ChromeOptions { ImplicitWaitTimeout = 10.Seconds() };
-        options.AddArguments("--no-sandbox", "--disable-extensions");
+        var options = new ChromeOptions
+        {
+            ImplicitWaitTimeout = defaultTimeout,
+            PageLoadTimeout = defaultTimeout,
+            ScriptTimeout = defaultTimeout
+        };
+        options.AddArguments("--no-sandbox", "--disable-extensions", "--window-size=1200,1200");
 
         driver = new ChromeDriver(options);
-        driver.Manage().Window.Size = new Size(1200, 1200);
+        waiter = new WebDriverWait(driver, defaultTimeout);
         Authorization();
     }
 
@@ -31,140 +40,142 @@ public class SeleniumTestsForPractice
     }
 
     [Test]
-    public void Should_Authorize()
-    {
-        var news = driver.FindElement(By.CssSelector("[data-tid='Title']"));
-
-        var currentUrl = driver.Url;
-        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
-    }
-
-    [Test]
     public void Should_navigate()
     {
-        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
-        sideMenu.Click();
+        OpenSideMenu();
+        
         var community = driver.FindElements(By.CssSelector("[data-tid='Community']"))
             .First(element => element.Displayed);
         community.Click();
         var communityTitle = driver.FindElement(By.CssSelector("[data-tid='Title']"));
-        Assert.That(driver.Url == "https://staff-testing.testkontur.ru/communities");
+        
+        driver.Url.Should().Be($"{baseUrl}communities");
     }
 
     [Test]
     public void Should_create_folder()
     {
-        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
-        sideMenu.Click();
+        OpenSideMenu();
+        
         var files = driver.FindElements(By.CssSelector("[data-tid='Files']"))
             .First(element => element.Displayed);
         files.Click();
-        var dropdownButton = driver.FindElements(By.CssSelector("section")).First(e =>
-                e.FindElements(By.CssSelector("[data-tid = 'DropdownButton']")).Count != 0)
-            .FindElement(By.CssSelector("[data-tid = 'DropdownButton']"));
-        dropdownButton.Click();
-        var createFolder = driver.FindElement(By.CssSelector("[data-tid = 'CreateFolder']"));
-        createFolder.Click();
-        var input = driver.FindElement(By.CssSelector("[data-tid='Input']"));
-        var folderName = GenerateString();
-        input.SendKeys(folderName);
-        var saveButton = driver.FindElement(By.CssSelector("[data-tid='SaveButton']"));
-        saveButton.Click();
+        ClickOnElementByCssSelector("section [data-tid='DropdownButton']");
+        ClickOnElementByCssSelector("[data-tid='CreateFolder']");
+        var folderNameInput = driver.FindElement(By.CssSelector("[data-tid='Input']"));
+        var folderName = GenerateGuidString();
+        folderNameInput.SendKeys(folderName);
+        ClickOnElementByCssSelector("[data-tid='SaveButton']");
         var createdFolder = driver.FindElements(By.XPath($"//div[contains(text(), '{folderName}')]"));
+        
         createdFolder.Count.Should().Be(1);
     }
 
     [Test]
     public void Should_navigate_to_community_where_I_Member()
     {
-        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
-        sideMenu.Click();
+        OpenSideMenu();
         var community = driver.FindElements(By.CssSelector("[data-tid='Community']"))
             .First(element => element.Displayed);
         community.Click();
-        Task.Delay(2.Seconds()).GetAwaiter().GetResult();
-        var item = driver.FindElement(By.CssSelector("[data-tid='PageHeader']"))
-            .FindElements(By.CssSelector("[data-tid='Item']"))
-            .First(element => element.Text == "Я участник");
-        item.Click();
-        Assert.That(driver.Url == "https://staff-testing.testkontur.ru/communities?activeTab=isMember");
+
+        waiter.Until(ExpectedConditions.ElementExists(By.CssSelector("[data-tid='CommunitiesCounter']")));
+        var iMemberXPath = "//*[@data-tid='PageHeader']//*[@data-tid='Tabs']//*[@data-tid='Item'][contains(text(), 'Я участник')]";
+        waiter.Until(ExpectedConditions.ElementIsVisible(By.XPath(iMemberXPath)));
+        var iMemberTab = driver.FindElement(By.XPath(iMemberXPath));
+        iMemberTab.Click();
+
+        waiter.Until(ExpectedConditions.UrlToBe($"{baseUrl}communities?activeTab=isMember"));
+        waiter.Until(ExpectedConditions.ElementExists(By.CssSelector("[data-tid='CommunitiesCounter']")));
+        
+        var iMemberElements = driver.FindElement(By.CssSelector("[data-tid='Feed']"))
+            .FindElements(By.XPath("//span[contains(text(), 'Я участник')]"));
+        var communityCount = driver.FindElement(By.CssSelector("[data-tid='CommunitiesCounter']"))
+            .FindElement(By.TagName("span")).Text.Split(" ").First();
+        int.Parse(communityCount).Should().Be(iMemberElements.Count);
     }
 
     [Test]
     public void Should_navigate_to_employers()
     {
-        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
-        sideMenu.Click();
+        OpenSideMenu();
+        
         var companyButton = driver.FindElements(By.CssSelector("[data-tid='Structure']"))
             .First(element => element.Displayed);
         companyButton.Click();
-        var company = driver.FindElement(By.CssSelector("[data-tid='DepartmentList']"))
-            .FindElements(By.CssSelector("[data-tid='Item']")).First();
-        company.Click();
-        var item = driver.FindElement(By.CssSelector("[data-tid='PageHeader']"))
-            .FindElements(By.CssSelector("[data-tid='Item']"))
+        ClickOnElementByCssSelector("[data-tid='DepartmentList'] [data-tid='Item']");
+        var employersTab = driver.FindElements(By.CssSelector("[data-tid='PageHeader'] [data-tid='Item']"))
             .First(element => element.Text == "Сотрудники");
-        item.Click();
-
-        Assert.That(driver.Url.Contains("tab=employees"));
+        employersTab.Click();
+        
+        driver.Url.Should().Contain("tab=employees");
     }
 
     [Test]
-    // Зайти в раздел "Комментарии" и написать комментарий "Автотест Насти Останиной"
     public void Should_comment_from_comments_page()
     {
-        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
-        sideMenu.Click();
-        var commentsButton = driver.FindElements(By.CssSelector("[data-tid='Comments']"))
+        OpenSideMenu();
+        var commentsButton = driver.FindElements(By.CssSelector("a[data-tid='Comments']"))
             .First(element => element.Displayed);
         commentsButton.Click();
 
+        ClickOnElementByCssSelector("[data-tid='AddComment']");
         var addCommentElement = driver.FindElement(By.CssSelector("[data-tid='AddComment']"));
         addCommentElement.Click();
         var commentInputField = driver.FindElement(By.TagName("textarea"));
-        var comment = GenerateString();
+        var comment = GenerateGuidString();
         commentInputField.SendKeys(comment);
-        var sendCommentButton = driver.FindElement(By.CssSelector("[data-tid='SendComment']"));
-        sendCommentButton.Click();
+        ClickOnElementByCssSelector("[data-tid='SendComment']");
 
         var existedComment = driver.FindElements(By.XPath($"//div[contains(text(), '{comment}')]"));
         existedComment.Count.Should().Be(1);
     }
 
     [Test]
-    //   Зайти в раздел "Редактировать" и указать свое рабочее место, нажать на кнопку "Сохранить"
     public void Should_edit_address()
     {
-        var avatarButton = driver.FindElement(By.CssSelector("[data-tid='Avatar']"));
-        avatarButton.Click();
-        var editButton = driver.FindElement(By.CssSelector("[data-tid='ProfileEdit']"));
-        editButton.Click();
-        var address = GenerateString();
-        var addressInputField = driver.FindElement(By.CssSelector("[data-tid='Address']"))
-            .FindElement(By.TagName("textarea"));
+        ClickOnElementByCssSelector("[data-tid='Avatar']");
+        ClickOnElementByCssSelector("[data-tid='ProfileEdit']");
+        var address = GenerateGuidString();
+        var addressInputField = driver.FindElement(By.CssSelector("[data-tid='Address'] textarea"));
         addressInputField.SendKeys(address);
 
-        var saveButton = driver.FindElement(By.CssSelector("[data-tid='PageHeader'")).FindElements(By.TagName("button"))
+        var saveButton = driver.FindElements(By.CssSelector("[data-tid='PageHeader'] button"))
             .First(e => e.Text == "Сохранить");
         saveButton.Click();
 
-        var savedAddress = driver.FindElements(By.XPath($"//div[contains(text(), '{address}')]"));
+        var savedAddress = driver.FindElements(By.XPath($"//div[contains(text(),'{address}')]"));
         savedAddress.Count.Should().Be(1);
+    }
+
+    private void OpenSideMenu()
+    {
+        ClickOnElementByCssSelector("[data-tid='SidebarMenuButton']");
+    }
+
+    private void ClickOnElementByCssSelector(string selector)
+    {
+        waiter.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(selector)));
+        var element = driver.FindElement(By.CssSelector(selector));
+
+        element.Click();
     }
 
     private void Authorization()
     {
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys(SeleniumTestsForPractice.login);
-        var password = driver.FindElement(By.Name("Password"));
-        password.SendKeys(SeleniumTestsForPractice.password);
+        driver.Navigate().GoToUrl(baseUrl);
+        var loginField = driver.FindElement(By.Id("Username"));
+        loginField.SendKeys(login);
+        var passwordField = driver.FindElement(By.Name("Password"));
+        passwordField.SendKeys(password);
 
-        var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
+        ClickOnElementByCssSelector("[name='button']");
+
+        var newsTitle = driver.FindElement(By.CssSelector("[data-tid='Title']"));
+        newsTitle.Text.Should().Be("Новости");
     }
 
-    private static string GenerateString()
+    private static string GenerateGuidString()
     {
         return Guid.NewGuid().ToString();
     }
